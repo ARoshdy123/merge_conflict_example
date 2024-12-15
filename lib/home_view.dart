f29ad4e8c30717362b7c520edc0c74832ccd1ceb
Ahmed Roshdy <141560553+ARoshdy123@users.noreply.github.com>
2024-12-15 23:52:14 +0200

    Merge pull request #2
    * adding home_view.dart
    
    * adding home_view.dart

---
+++ lib/home_view.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 20,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.green
+      ),
+      child: SizedBox(),
+    );
+  }
+}
