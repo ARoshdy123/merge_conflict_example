e2d6980ad8c35d726418f4d52697161f93e76cae
AhmedRoshdy <ahmedroshdy6555554@gmail.com>
2024-12-15 23:32:30 +0200

    Initial Comit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
+    );
+  }
+}
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red
+      ),
+      child: SizedBox(),
+    );
+  }
+}
+
